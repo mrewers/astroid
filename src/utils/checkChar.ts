42fb5d598b007743a69f31727884a1a17f841a7e
Marek Rewers <hello@marek.dev>
2021-11-07 01:49:42 -0500

    Refactor(parser): Export each character function individually

--- src/utils/checkChar.ts
+++ src/utils/checkChar.ts
@@ -1,21 +1,23 @@
 const LETTER = /[a-zA-Z]/u;
 const WHITESPACE = /\s+/u;
 const NUMBER = /^[0-9]+$/u;
-const OPERATORS = ['+', '-', '*', '/', '%', '=', '!', '&', '|', '>', '<'];
+const OPERATORS = ['+', '-', '*', '/', '%', '!', '&', '|', '>', '<'];
 
-const isLetter = (char: string): boolean => LETTER.test(char);
-const isNumber = (char: string): boolean => NUMBER.test(char);
-const isOperator = (char: string): boolean => OPERATORS.includes(char);
-const isWhiteSpace = (char: string): boolean => WHITESPACE.test(char);
+// Basic character types
+export const isLetter = (char: string): boolean => LETTER.test(char);
+export const isNumber = (char: string): boolean => NUMBER.test(char);
+export const isOperator = (char: string): boolean => OPERATORS.includes(char);
+export const isWhiteSpace = (char: string): boolean => WHITESPACE.test(char);
 
-const isBracesOpen = (char: string): boolean => char === '{';
-const isBracesClose = (char: string): boolean => char === '}';
+// Parenthetical characters
+export const isBracesOpen = (char: string): boolean => char === '{';
+export const isBracesClose = (char: string): boolean => char === '}';
 
-const isBracketOpen = (char: string): boolean => char === '[';
-const isBracketClose = (char: string): boolean => char === ']';
+export const isBracketOpen = (char: string): boolean => char === '[';
+export const isBracketClose = (char: string): boolean => char === ']';
 
-const isParenOpen = (char: string): boolean => char === '(';
-const isParenClose = (char: string): boolean => char === ')';
+export const isParenOpen = (char: string): boolean => char === '(';
+export const isParenClose = (char: string): boolean => char === ')';
 
 const checkChar = {
   isBracesClose,
