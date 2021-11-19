a20819f75b8003ba71aa55f4ab1b183fbd9d8f1e
Marek Rewers <hello@marek.dev>
2021-11-19 01:00:10 -0500

    Refactor(parser): Add helpers to simplify typing

--- src/parser/analyzer/blocks.ts
+++ src/parser/analyzer/blocks.ts
@@ -1,31 +1,41 @@
 import { isBlockOpen, isBlockClose } from '../utils/checkToken';
 import { first, peek } from '../../fp/fp';
+import type { IFunctionDeclaration } from './functions';
 import type { IToken } from '../lexer/tokenize';
+import type { ILoopReturn } from './analyzeSyntax';
 
 interface IBlockStatement {
   readonly type: string;
   readonly start: number;
   readonly end: number;
-  readonly body: IBlockStatement[] | IToken[];
+  readonly body: Array<IBlockStatement | IFunctionDeclaration | IToken>;
 }
 
-interface ILoopInfo {
-  readonly value: IBlockStatement | IToken;
-  readonly remaining: IBlockStatement[] | IToken[];
-}
+/* Adjust the first and peek functions to facilitate typing */
+const _first = (ts: IToken[]): IBlockStatement | IToken => first(ts) as IBlockStatement | IToken;
+const _peek = (ts: IToken[]): IBlockStatement | IToken => peek(ts) as IBlockStatement | IToken;
 
-const blockStatements = (tokens: IToken[]): ILoopInfo => {
-  const token = pop(tokens) as IToken;
+/**
+ * Look for block statements (i.e. content wrapped in curly braces) amongst the tokens.
+ * @param tokens - A list of tokenized code elements.
+ */
+const blockStatements = (tokens: IToken[]): ILoopReturn => {
+  const token = _first(tokens);
 
   if (isBlockOpen(token.type)) {
-    let { end } = token;
     const body = [];
+    let { end } = token;
 
-    while (!isBlockClose((peek(tokens) as IToken).type)) {
+    // Populate the block body with it's tokens.
+    // We do so recursively to account for nested blocks.
+    while (!isBlockClose(_peek(tokens).type)) {
       body.push(blockStatements(tokens).value);
-      end = (peek(tokens) as IToken).end;
+
+      // Set the block end to the position of the closing brace.
+      end = _peek(tokens).end;
     }
 
+    // Discard the closing bracket.
     first(tokens);
 
     const statement = {
