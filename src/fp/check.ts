52efa85468ebc13dff5df78aa994eb159caf935a
Marek Rewers <hello@marek.dev>
2021-11-20 00:35:06 -0500

    Feat(fp): Add helper function to check if something is an Object

---
+++ src/fp/check.ts
@@ -0,0 +1,7 @@
+/**
+ * Checks whether an item is an object.
+ * @param obj - The possible object.
+ */
+const isObject = (obj: unknown): boolean => obj instanceof Object;
+
+export default isObject;
