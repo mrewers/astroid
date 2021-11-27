d27360770d09b46a1f2800d720ccd30b467eaa58
Marek Rewers <hello@marek.dev>
2021-11-26 22:45:33 -0500

    Test(fp): Allow the use of any in generic rules

--- packages/fp/arrays.ts
+++ packages/fp/arrays.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export const peek = (array: any[]): any => array[0];
 
 export const first = (array: any[]): any => array.shift();
-
-/* eslint-enable */
