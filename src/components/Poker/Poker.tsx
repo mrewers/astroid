69bdd718eb0691aaf3d17ea5cffed85af8f0aa90
Marek Rewers <hello@marek.dev>
2021-12-23 14:25:18 -0500

    Feat(site): Add control panel and card layout to poker homepage

--- src/components/Poker/Poker.tsx
+++ src/components/Poker/Poker.tsx
@@ -1,20 +1,45 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import { getTheDeck } from '@mrewers/poker/constants';
 import type { FunctionalComponent } from 'preact';
 
 import BettingRange from './BettingRange/BettingRange';
+import ControlPanel from '../ControlPanel/ControlPanel';
+import PlayingCard from './PlayingCard/PlayingCard';
 import style from './Poker.module.scss';
+
+const pokerOptions = [
+  { label: 'Select Tool', value: '' },
+  { label: 'Betting Range', value: 'betting' },
+];
 
 /**
  * A JSX component that renders the poker tools page.
  * @component
  */
-const Poker: FunctionalComponent = () => (
-  <div className={style.container}>
-    <div className={style.range}>
-      <BettingRange />
+const Poker: FunctionalComponent = () => {
+  const [view, setView] = useState('');
+
+  const deck = getTheDeck();
+
+  return (
+    <div className={style.container}>
+      <ControlPanel current={view} opts={pokerOptions} switcher={setView} />
+      {view === '' && (
+        <div className={style.cards}>
+          {deck.map(card => (
+            <PlayingCard key={`${card.suit}-${card.rank}`} suit={card.suit} value={card.value} />
+          ))}
+        </div>
+      )}
+      {view === 'betting' && (
+        <div className={style.range}>
+          <BettingRange />
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 Poker.displayName = 'Page - Poker';
 
