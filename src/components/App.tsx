4d5fe8a875876e3727919058f6d156765a32a014
Marek Rewers <hello@marek.dev>
2022-01-07 23:49:28 -0500

    Feat(site): Dynamically update the document title upon navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import { Router } from 'preact-router';
 import type { FunctionalComponent } from 'preact';
+import type { RouterOnChangeArgs } from 'preact-router';
 
 import Algos from './Algos/Algos';
 import Astroid from './Astroid/Astroid';
@@ -13,22 +15,51 @@ import style from './App.module.scss';
  * A JSX component that renders the application.
  * @component
  */
-const App: FunctionalComponent = () => (
-  <div>
-    <header>
-      <h1 className={style.title}>ASTroid</h1>
-      <Nav />
-    </header>
-    <main>
-      <Router>
-        <Home path="/" />
-        <Astroid path="/ast" />
-        <Algos path="/algos" />
-        <Poker path="/poker" />
-      </Router>
-    </main>
-  </div>
-);
+const App: FunctionalComponent = () => {
+  const [title, setTitle] = useState('ASTroid');
+
+  /**
+   * Update the page and document title when the user navigates to a new page.
+   * @param e - An on change event emitted by the Preact router.
+   */
+  const updateTitle = (e: RouterOnChangeArgs): void => {
+    const setNewTitle = (value: string): void => {
+      setTitle(value);
+      document.title = value.toLocaleLowerCase() === 'astroid' ? value : `${value} | Astroid`;
+    };
+
+    switch (e.url) {
+      case '/algos':
+        setNewTitle('Algorithms');
+        break;
+      case '/ast':
+        setNewTitle('AST');
+        break;
+      case '/poker':
+        setNewTitle('Poker');
+        break;
+      default:
+        setNewTitle('ASTroid');
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <h1 className={style.title}>{title}</h1>
+        <Nav />
+      </header>
+      <main>
+        <Router onChange={(e): void => updateTitle(e)}>
+          <Home path="/" />
+          <Astroid path="/ast" />
+          <Algos path="/algos" />
+          <Poker path="/poker" />
+        </Router>
+      </main>
+    </div>
+  );
+};
 
 App.displayName = 'App';
 
