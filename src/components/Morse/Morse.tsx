c306c20e439605ef61616f653b410b3b274714e5
Marek Rewers <hello@marek.dev>
2022-01-12 23:14:47 -0500

    Feat(site): Add a page for the Morse code components

---
+++ src/components/Morse/Morse.tsx
@@ -0,0 +1,36 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import type { FunctionalComponent } from 'preact';
+
+import Alphabet from './Alphabet/Alphabet';
+import ControlPanel from '../ControlPanel/ControlPanel';
+import Decoder from './Decoder/Decoder';
+import Legend from './Legend/Legend';
+import style from './Morse.module.scss';
+
+const pokerOptions = [
+  { label: 'Decoder', value: '' },
+  { label: 'The Alphabet', value: 'alpha' },
+];
+
+/**
+ * A JSX component that renders the Morse code page.
+ * @component
+ */
+const Morse: FunctionalComponent = () => {
+  const [view, setView] = useState('');
+
+  return (
+    <div className={style.container}>
+      <ControlPanel current={view} opts={pokerOptions} switcher={setView}>
+        <Legend />
+      </ControlPanel>
+      {view === '' && <Decoder mode="text" />}
+      {view === 'alpha' && <Alphabet />}
+    </div>
+  );
+};
+
+Morse.displayName = 'Page - Morse Code';
+
+export default Morse;
